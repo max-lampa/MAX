2a34ce71cf004448d885de4f649bd9cd23da8386
max-lampa <max.github.io@gmail.com>
2026-03-22 09:37:40 +0200

    Add files via upload

---
+++ max-radio.js
@@ -0,0 +1,150 @@
+#EXTM3U
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",NEOCLASSICAL METAL
+http://79.111.119.111:8000/neoclassicalmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",PAGAN METAL
+http://79.111.119.111:8000/paganmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",SYMPHONIC BLACK METAL
+http://79.111.119.111:8000/symphonicblackmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",GROOVE METAL
+http://79.120.39.202:8000/groovemetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",SYMPHONIC METAL / SYMPHOMETAL
+http://79.120.77.11:8000/symphometal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",DEATH DOOM METAL
+http://79.120.39.202:8000/deathdoom
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",DARK METAL
+http://79.111.119.111:8000/darkmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",DOOM METAL
+http://79.111.14.76:8000/doom
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",POST / INSTRO / ART METAL
+http://79.120.39.202:8000/postmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",PROGRESSIVE BLACK / POST-BLACK / BLACKGAZE
+http://79.111.14.76:8000/postblack
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",GOTHIC DOOM METAL
+http://79.120.39.202:8000/gothdoommet
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",PROGRESSIVE POWER METAL
+http://79.111.119.111:8000/progpowermetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",NWOBHM
+http://79.120.12.130:8000/nwobhm
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",NEUE DEUTSCHE HARTE
+http://213.141.131.10:8000/ndh
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",VIKING METAL
+http://79.111.14.76:8000/vikingmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",DEATH'N'ROLL
+http://79.120.39.202:8000/deathnroll
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",FOLK BLACK METAL
+http://79.120.12.130:8000/folkblackmet
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",BLACK METAL
+http://79.120.77.11:8000/blackmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",DEATH METAL
+http://79.120.77.11:8000/deathmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",PROGRESSIVE DEATH METAL
+http://79.111.119.111:8000/progdeath
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",NU METAL
+http://79.111.14.76:8000/numetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",SLUDGE METAL
+http://79.120.77.11:8000/sludgemetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",POWER METAL
+http://79.120.77.11:8000/powermetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",BLACK / BLACKENED DEATH METAL
+http://79.111.14.76:8000/blackdeath
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",INSTRUMENTAL METAL
+http://79.111.119.111:8000/instrumentalmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",AVANT-GARDE METAL
+http://79.111.14.76:8000/avantgardemetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",HEAVY POWER METAL
+http://79.120.12.130:8000/heavypowermetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",MELODIC HEAVY METAL
+http://79.111.119.111:8000/melodicheavy
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",SYMPHONIC POWER METAL
+http://213.141.131.10:8000/symphopower
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",SYMPHONIC GOTHIC METAL
+http://213.141.131.10:8000/symphogoth
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",MEDIEVAL METAL
+http://213.141.131.10:8000/medievalmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",JAZZ / FUSION METAL
+http://213.141.131.10:8000/jazzmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",MELODIC DEATH METAL
+http://79.120.77.11:8000/melodicdeath
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",GOTHIC BLACK METAL
+http://79.120.39.202:8000/gothblackmet
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",FOLK DEATH METAL
+http://79.120.12.130:8000/folkdeathmet
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",PROGRESSIVE METAL
+http://79.120.77.11:8000/progmetal
+#EXTINF:-1 group-title="METAL" tvg-logo="https://i.postimg.cc/SxYJ1hKh/c300.jpg",HEAVY METAL
+http://79.111.14.76:8000/heavymetal
+
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",RUSSIAN POP
+http://79.120.77.11:8000/popru
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",NU DISCO
+http://79.111.119.111:8000/nudisco
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",POP MUSIC OF THE USSR
+http://79.120.77.11:8000/ussr
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",CHINESE POP
+http://213.141.131.10:8000/cpop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",TURKISH POP
+http://213.141.131.10:8000/turkishpop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",REGGAETON
+http://213.141.131.10:8000/reggaeton
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",CANTOPOP
+http://79.120.12.130:8000/cantopop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",POP BALLADS
+http://79.120.77.11:8000/popballads
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",POP
+http://79.120.77.11:8000/pop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",ITALO DISCO
+http://79.111.119.111:8000/italodisco
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",JAPANESE POP
+http://79.111.119.111:8000/jpop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",FRENCH CHANSON
+http://79.111.14.76:8000/frenchchanson
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",FRENCH POP MUSIC
+http://79.120.12.130:8000/frenchpop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",KOREAN POP
+http://79.111.119.111:8000/kpop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",EURODANCE
+http://79.120.39.202:8000/eurodance
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",EURO DISCO
+http://79.111.119.111:8000/eurodisco
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",DISCO
+http://79.120.77.11:8000/disco
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",RUSSIAN POP 80-90s
+http://79.120.77.11:8000/80ru
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",ARABIC POP MUSIC
+http://79.120.39.202:8000/arabicpop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",LATIN POP
+http://79.111.119.111:8000/latinpop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",DANCE POP
+http://79.120.77.11:8000/dancepop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",ITALIAN POP
+http://213.141.131.10:8000/italopop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",FOLK POP
+http://213.141.131.10:8000/folkpop
+#EXTINF:-1 group-title="POP" tvg-logo="https://i.postimg.cc/ZRk0745r/pop.jpg",CLASSIC POP
+http://213.141.131.10:8000/tradpop
+
+#EXTINF:-1 group-title="ROCK" tvg-logo="https://i.postimg.cc/pLM7Lj7z/2023-04-10-200831163.png",PSYCHOBILLY
+http://79.111.119.111:8000/psychobilly
+#EXTINF:-1 group-title="ROCK" tvg-logo="https://i.postimg.cc/pLM7Lj7z/2023-04-10-200831163.png",INDIE
+http://79.111.119.111:8000/indiepop
+#EXTINF:-1 group-title="ROCK" tvg-logo="https://i.postimg.cc/pLM7Lj7z/2023-04-10-200831163.png",FOLK ROCK
+http://79.111.14.76:8000/folkrock
+#EXTINF:-1 group-title="ROCK" tvg-logo="https://i.postimg.cc/pLM7Lj7z/2023-04-10-200831163.png",ROCK'n'ROLL
+http://79.120.77.11:8000/rockroll
+#EXTINF:-1 group-title="ROCK" tvg-logo="https://i.postimg.cc/pLM7Lj7z/2023-04-10-200831163.png",HARD ROCK
+http://79.120.77.11:8000/hardrock
+#EXTINF:-1 group-title="ROCK" tvg-logo="https://i.postimg.cc/pLM7Lj7z/2023-04-10-200831163.png",RUSSIAN ROCK
+http://79.120.77.11:8000/rockru
+#EXTINF:-1 group-title="ROCK" tvg-logo="https://i.postimg.cc/pLM7Lj7z/2023-04-10-200831163.png",PUNK ROCK
+http://79.120.77.11:8000/punk
+#EXTINF:-1 group-title="ROCK" tvg-logo="https://i.postimg.cc/pLM7Lj7z/2023-04-10-200831163.png",RUSSIAN PUNK ROCK
+http://79.120.77.11:8000/punkru
+
+#EXTINF:-1 group-title="SHANSON" tvg-logo="https://i.postimg.cc/Pfg61r8d/dostavka-shanson-ekb.jpg",RUSSIAN SHANSON
+http://79.120.77.11:8000/shansonru
+#EXTINF:-1 group-title="SHANSON" tvg-logo="https://i.postimg.cc/Pfg61r8d/dostavka-shanson-ekb.jpg",RUSSIAN POST SHANSON
+http://79.120.77.11:8000/postshanson
+#EXTINF:-1 group-title="SHANSON" tvg-logo="https://i.postimg.cc/Pfg61r8d/dostavka-shanson-ekb.jpg",GYPSY ROMANCES
+http://79.120.77.11:8000/gypsyru
+#EXTINF:-1 group-title="SHANSON" tvg-logo="https://i.postimg.cc/Pfg61r8d/dostavka-shanson-ekb.jpg",RUSSIAN ROMANCE
+http://79.120.77.11:8000/romanceru
